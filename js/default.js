3f8bb376fab209e654fae5f14828a949effa320d
luizinbrzado <luiztrineves@gmail.com>
2021-10-03 12:24:20 -0300

    Criando apresentação

--- js/default.js
+++ js/default.js
@@ -14,7 +14,7 @@ let footer = document.querySelector('footer');
 function footerNoDom() {
     let footerHTML =
     `<a href="https://github.com/luizinbrzado" target="_blank" rel="noopener"><i class="fab fa-github-square fa-3x"></i></a>
-    <p><span>&#169</span> Copyright -  Feito com &#128151 por <a href="https://beacons.ai/luizinbrzado" target="_blank" rel="noopener">luizinbrzado</a></p>
+    <p><span>&#169</span> Copyright -  Feito com &#128151 por <a class="link link--metis" href="https://beacons.ai/luizinbrzado" target="_blank" rel="noopener">luizinbrzado</a></p>
     <a href="https://www.linkedin.com/in/luizinbrzado/" target="_blank" rel="noopener"><i class="fab fa-linkedin fa-3x"></i></a>`
 
     footer.innerHTML += footerHTML;
